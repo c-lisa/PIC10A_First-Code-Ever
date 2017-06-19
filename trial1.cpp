94e6967442fde9382093bc80da5049b6988e014f
c-lisa <clisa928@gmail.com>
2017-06-19 15:20:33 -0700

    Add files via upload

---
+++ trial1.cpp
@@ -0,0 +1,23 @@
+/*this is exciting because I can code on my laptop now!*/
+
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cmath>
+
+using namespace std; 
+
+int main()
+{
+	string a; 
+	string b; 
+	
+	a = "Hello world!"; 
+	b = "Goodbye world!"; 
+
+	cout << a << endl; 
+	cout << b << endl; 
+
+	system("pause"); 
+	return 0;
+}
